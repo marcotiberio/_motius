361fe0582541c676e7b36b6b463fc9183dc0124f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-30 18:29:39 +0200

    test

--- home.php
+++ home.php
@@ -81,14 +81,12 @@ get_header();
 				$section9 = get_field('section9');
 				if( $section9 ): ?>
 						<div class="left">
-							<?php echo $section9['title_left']; ?>
+							<?php echo $section9['label_left']; ?>
 							<?php echo $section9['text_left']; ?>
-							<?php echo $section9['button_left']; ?>
 						</div>
 						<div class="right">
-						<?php echo $section9['title_right']; ?>
+							<?php echo $section9['label_right']; ?>
 							<?php echo $section9['text_right']; ?>
-							<?php echo $section9['button_right']; ?>
 						</div>
 				<?php endif; ?>
 		</section>
