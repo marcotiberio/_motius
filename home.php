591558339e19456700578348167420f70aefddd6
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-30 18:07:36 +0200

    test

--- home.php
+++ home.php
@@ -13,89 +13,56 @@ get_header();
 
 	<main id="primary" class="site-main">
 
+		<section id="one">
+
+		</section>
+		<section class="two-columns" id="two">
 		<?php
-		if ( have_posts() ) :
+			$section2 = get_field('section2');
+			if( $section2 ): ?>
+					<div class="text">
+						<?php echo $section2['text']; ?>
+					</div>
+					<div class="image" style="background-image:url('<?php echo esc_url($section2['image']['url']); ?>');"></div>
+			<?php endif; ?>
+		</section>
+		<section class="two-columns" id="three">
+		<?php
+			$section3 = get_field('section3');
+			if( $section3 ): ?>
+					<div class="text">
+						<?php echo $section3['text']; ?>
+					</div>
+					<div class="image" style="background-image:url('<?php echo esc_url($section3['image']['url']); ?>');"></div>
+			<?php endif; ?>
+		</section>
+		<section class="two-columns" id="four">
+		<?php
+			$section4 = get_field('section4');
+			if( $section4 ): ?>
+					<div class="text">
+						<?php echo $section4['text']; ?>
+					</div>
+					<div class="image" style="background-image:url('<?php echo esc_url($section4['image']['url']); ?>');"></div>
+			<?php endif; ?>
+		</section>
+		<section class="two-columns" id="five">
+			
+		</section>
+		<section id="six">
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+		</section>
+		<section id="seven">
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		</section>
+		<section class="two-columns" id="eight">
+			
+		</section>
+		<section id="nine">
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+		</section>
 
 	</main><!-- #main -->
-
-	<section id="one">
-
-	</section>
-	<section class="two-columns" id="two">
-	<?php
-		$section2 = get_field('section2');
-		if( $section2 ): ?>
-				<div class="text">
-					<?php echo $section2['text']; ?>
-				</div>
-				<div class="image" style="background-image:url('<?php echo esc_url($section2['image']['url']); ?>');"></div>
-		<?php endif; ?>
-	</section>
-	<section class="two-columns" id="three">
-	<?php
-		$section3 = get_field('section3');
-		if( $section3 ): ?>
-				<div class="text">
-					<?php echo $section3['text']; ?>
-				</div>
-				<div class="image" style="background-image:url('<?php echo esc_url($section3['image']['url']); ?>');"></div>
-		<?php endif; ?>
-	</section>
-	<section class="two-columns" id="four">
-	<?php
-		$section4 = get_field('section4');
-		if( $section4 ): ?>
-				<div class="text">
-					<?php echo $section4['text']; ?>
-				</div>
-				<div class="image" style="background-image:url('<?php echo esc_url($section4['image']['url']); ?>');"></div>
-		<?php endif; ?>
-	</section>
-	<section class="two-columns" id="five">
-		
-	</section>
-	<section id="six">
-
-	</section>
-	<section id="seven">
-
-	</section>
-	<section class="two-columns" id="eight">
-		
-	</section>
-	<section id="nine">
-
-	</section>
 
 <?php
 get_sidebar();
