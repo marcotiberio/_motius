20b520e3a0366408454200acff85dcd193fc903e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-01 13:47:25 +0200

    bg colors

--- companies.php
+++ companies.php
@@ -15,11 +15,11 @@ get_header();
 		<?php
 			$section1 = get_field('section1');
 			if( $section1 ): ?>
-				<section id="one" style="background-color:<?php echo $section1['background_color']; ?>">
+				<section id="one" style="background-color:<?php echo $section1['background_color_section']; ?>">
 					<div class="hero-text">
 						<h1 class="header"><?php echo $section1['header']; ?></h1>
 						<p class="paragraph"><?php echo $section1['paragraph']; ?></p>
-						<div class="block">
+						<div class="block" style="background-color:<?php echo $section1['background_color_block']; ?>">>
 							<h3 class="header-block"><?php echo $section1['header_block']; ?></h3>
 							<p class="paragraph-block"><?php echo $section1['paragraph_block']; ?></p>
 						</div>
