87db50e89dd9ed80154d86b0b94d0958ecf2cc91
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-01 16:29:08 +0200

    about template

---
+++ about.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * This is the About template
+ * and one of the two required files for a theme (the other being style.css).
+ *
+ * @package _motius
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<?php
+			$section1 = get_field('section1');
+			if( $section1 ): ?>
+				<section id="one" style="background-color:<?php echo $section1['background_color_section']; ?>">
+					<div class="hero-text">
+						<h1 class="header"><?php echo $section1['header']; ?></h1>
+						<p class="paragraph"><?php echo $section1['paragraph']; ?></p>
+					</div>
+				</section>
+			<?php endif; ?>
+		</section>
+		<section id="two">
+		</section>
+		<section id="three">
+		</section>
+		<section id="four">
+		</section>
+		<section id="five">
+		</section>
+		<section id="six">
+		</section>
+		<section id="seven">
+		</section>
+		<section id="eight">
+		</section>
+		<section id="nine">
+		</section>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
