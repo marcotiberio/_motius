62ea28e583e8eb2ff7cc88bdfdbc842fc37e1e60
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-02 12:53:41 +0200

    fix list

--- companies.php
+++ companies.php
@@ -35,26 +35,24 @@ get_header();
 			<div class="two-column-list">
 				<h3 class="section-title">Benefits in a sentence formatting</h3>
 				<?php if( have_rows('section4') ): ?>
-					<div class="two-column-list">
-						<h3 class="section-title"><?php the_sub_field('section_title'); ?></h3>
-						<?php while( have_rows('section4') ): the_row(); 
+					<?php while( have_rows('section4') ): the_row(); 
 
-							// Load sub field value.
-							$sectiontitle = get_sub_field('section_title');
-							$iconlist = get_sub_field('icon_list');
-							$headerlist = get_sub_field('header_list');
-							$paragraphlist = get_sub_field('paragraph_list');
-							?>
-							<ul>
-								<li>
-									<img class="icon-list" src="<?php echo esc_url( $iconlist['url'] ); ?>" alt="<?php echo esc_attr( $iconlist['alt'] ); ?>" />
-									<p class="header-list"><?php the_sub_field('header_list'); ?></p>
-									<p class="paragraph-list"><?php the_sub_field('paragraph_list'); ?></p>
-								</li>
-							</ul>
-						<?php endwhile; ?>
-					</div>
+						// Load sub field value.
+						$sectiontitle = get_sub_field('section_title');
+						$iconlist = get_sub_field('icon_list');
+						$headerlist = get_sub_field('header_list');
+						$paragraphlist = get_sub_field('paragraph_list');
+						?>
+						<ul>
+							<li>
+								<img class="icon-list" src="<?php echo esc_url( $iconlist['url'] ); ?>" alt="<?php echo esc_attr( $iconlist['alt'] ); ?>" />
+								<p class="header-list"><?php the_sub_field('header_list'); ?></p>
+								<p class="paragraph-list"><?php the_sub_field('paragraph_list'); ?></p>
+							</li>
+						</ul>
+					<?php endwhile; ?>
 				<?php endif; ?>
+			</div>
 		</section>
 			<?php
 				$section5 = get_field('section5');
