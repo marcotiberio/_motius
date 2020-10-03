eb5cb24505661bd924a505c6e5f7c527660b56d7
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-03 13:49:51 +0200

    update home sections

--- home.php
+++ home.php
@@ -45,21 +45,23 @@ get_header();
 		<?php
 			$section3 = get_field('section3');
 			if( $section3 ): ?>
-				<section class="two-columns" id="three" style="background-image:url('<?php echo esc_url($section3['image']['url']); ?>');">
+				<section class="two-columns" id="three">
 					<div class="text">
 						<h3 class="header"><?php echo $section3['header']; ?></h3>
 						<p class="paragraph"><?php echo $section3['paragraph']; ?></p>
 					</div>
+					<div class="image" style="background-image:url('<?php echo esc_url($section3['image']['url']); ?>');"></div>
 				</section>
 			<?php endif; ?>
 		<?php
 			$section4 = get_field('section4');
 			if( $section4 ): ?>
-				<section class="two-columns" id="four" style="background-image:url('<?php echo esc_url($section4['image']['url']); ?>');">
+				<section class="two-columns" id="three">
 					<div class="text">
 						<h3 class="header"><?php echo $section4['header']; ?></h3>
 						<p class="paragraph"><?php echo $section4['paragraph']; ?></p>
 					</div>
+					<div class="image" style="background-image:url('<?php echo esc_url($section4['image']['url']); ?>');"></div>
 				</section>
 			<?php endif; ?>
 		</section>
