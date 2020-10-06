83c52a8216e455e55e6b982767bc69e3e6b3c73a
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-06 08:42:15 +0200

    link home in footer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="primary-footer">
-			<div class="logo"><img src="http://motius.local/wp-content/uploads/2020/09/logo-footer.png" alt=""></div>
+			<div class="logo"><img src="http://motius.local/wp-content/uploads/2020/09/logo-footer.png" alt="" onclick="window.location.href='http://motius.local/'"></div>
 			<?php
 				wp_nav_menu(
 					array(
