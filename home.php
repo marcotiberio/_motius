1f4ffda76912bb342388ad0376b0434a751f8a2e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-16 09:19:47 +0200

    removed wrong script

--- home.php
+++ home.php
@@ -227,45 +227,6 @@ get_header();
 
 	</main><!-- #main -->
 
-	<script>
-		(function(){
-			function hasClass(el, cls) {
-				if (el.className.match('(?:^|\\s)'+cls+'(?!\\S)')) { return true; } 
-				}
-			function addClass(el, cls) {
-				if (!el.className.match('(?:^|\\s)'+cls+'(?!\\S)')){ el.className += ' '+cls; } 
-				}
-			function delClass(el, cls) {
-				el.className = el.className.replace(new RegExp('(?:^|\\s)'+cls+'(?!\\S)'),'');
-				}
-
-			function elementFromTop(elem, classToAdd, distanceFromTop, unit) {
-				var winY = window.innerHeight || document.documentElement.clientHeight,
-					distTop = elem.getBoundingClientRect().top,
-					distPercent = Math.round((distTop / winY) * 100),
-					distPixels = Math.round(distTop),
-					distUnit;
-				distUnit = unit == 'percent' ? distPercent : distPixels;
-				if (distUnit <= distanceFromTop) {
-					if (!hasClass(elem, classToAdd)) { addClass(elem, classToAdd); }
-					} else {
-					delClass(elem, classToAdd);
-					}
-				}
-			// params: element id, class to add, distance from top, unit ('percent' or 'pixels')
-
-			window.addEventListener('scroll', function() {
-				elementFromTop(document.getElementById('oneone'), 'element-from-top', 500, 'pixels');
-				}, false);
-			window.addEventListener('scroll', function() {
-				elementFromTop(document.getElementById('second'), 'element-from-top', 500, 'pixels');
-				}, false);
-			window.addEventListener('scroll', function() {
-				elementFromTop(document.getElementById('third'), 'element-from-top', 500, 'pixels');
-				}, false);
-		})();
-		</script>
-
 <?php
 get_sidebar();
 get_footer();
