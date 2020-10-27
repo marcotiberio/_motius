bc57a301c5223a31f01e89970aba245701fdf90b
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-10-27 15:53:27 +0100

    test

--- functions.php
+++ functions.php
@@ -154,7 +154,6 @@ function _motius_scripts() {
 	wp_enqueue_style( 'post-css', get_template_directory_uri() . '/css/post.css', 'all');
 	wp_enqueue_style( 'archive-css', get_template_directory_uri() . '/css/archive.css', 'all');
 	wp_enqueue_style( 'simple-text-css', get_template_directory_uri() . '/css/simple-text.css', 'all');
-	wp_enqueue_style( 'hamburger-css', get_template_directory_uri() . '/css/hamburger.css', 'all');
 	wp_enqueue_style( 'mobile-css', get_template_directory_uri() . '/css/mobile.css', 'all');
 	wp_style_add_data( '_motius-style', 'rtl', 'replace' );
 
